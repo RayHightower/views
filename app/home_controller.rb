a3b915fffebf92208e1dd5ad7776e0c278278444
Raymond T. Hightower <rth@wisdomgroup.com>
2012-11-05 18:09:07 -0700

    drop a button in there

--- app/home_controller.rb
+++ app/home_controller.rb
@@ -4,13 +4,22 @@ class HomeController < UIViewController
   end
 
   def viewDidLoad
-    self.title = "Home"
+    self.title = "Home Nav Controller"
     label = UILabel.alloc.initWithFrame([[15,100], [280,140]])
     label.text = "Hello Everybody!"
     label.font = UIFont.boldSystemFontOfSize(25)
     label.textColor = UIColor.blueColor
     label.textAlignment = UITextAlignmentCenter
+
+    button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
+    button.frame = [[15, 300], [200, 50]]
+    button.setTitle)"Move to next view", forState: UIControlStateNormal)
+    button.addTarget(self,
+                     action: "movetoChildView:",
+                     forControlEvents: UIControlEventTouchUpInside)
+
     view.addSubview(label)
+    view.addSubview(button)
   end
 
 end
