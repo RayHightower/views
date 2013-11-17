93b2349bb685cf594f8fe40f8972672c984251ff
Raymond T. Hightower <rth@wisdomgroup.com>
2013-11-17 12:15:22 -0600

    add awesome_print_motion

--- app/home_controller.rb
+++ app/home_controller.rb
@@ -5,24 +5,24 @@ class HomeController < UIViewController
 
   def viewDidLoad
     self.title = "Home Nav Controller"
-    label = UILabel.alloc.initWithFrame([[15,100], [280,140]])
+    label = UILabel.alloc.initWithFrame([[15,50], [280,140]])
     label.text = "Hello Everybody!"
     label.font = UIFont.boldSystemFontOfSize(25)
     label.textColor = UIColor.blueColor
     label.textAlignment = UITextAlignmentCenter
 
     button = UIButton.buttonWithType(UIButtonTypeRoundedRect)
-    button.frame = [[15, 300], [280, 50]]
+    button.frame = [[15, 250], [280, 50]]
     button.setTitle("Next view", forState: UIControlStateNormal)
     button.addTarget(self,
                      action: "moveToChildView:",
                      forControlEvents: UIControlEventTouchUpInside)
 
     button2 = UIButton.buttonWithType(UIButtonTypeRoundedRect)
-    button2.frame = [[15, 300], [280, 50]]
-    button2.setTitle("Next view", forState: UIControlStateNormal)
+    button2.frame = [[15, 350], [280, 50]]
+    button2.setTitle("Another view", forState: UIControlStateNormal)
     button2.addTarget(self,
-                     action: "moveToChildView:",
+                     action: "moveToAnotherChildView:",
                      forControlEvents: UIControlEventTouchUpInside)
 
     view.addSubview(label)
@@ -31,6 +31,7 @@ class HomeController < UIViewController
   end
 
   def moveToChildView(sender)
+    # It takes (sender) as a parameter so it knows what do do with the "back" button!!
     self.parentViewController.pushViewController(UIViewController.alloc.init,animated:true)
   end
 
